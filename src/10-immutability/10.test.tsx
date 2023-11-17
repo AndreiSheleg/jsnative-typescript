ce63f0f93858b5906ab1baa97dab2e48373f66d5
Andrei Sheleg <andrej.sheleg@gmail.com>
2023-11-17 04:52:07 +0300

    add new immutability test

--- src/10-immutability/10.test.tsx
+++ src/10-immutability/10.test.tsx
@@ -1,19 +1,96 @@
-import {UserType} from "./10";
+import {
+    makeHairStyle,
+    moveUser,
+    moveUserToOtherHouse,
+    upgradeUserLaptop,
+    UserType,
+    UserWithBooksType,
+    UserWithLaptopType
+} from "./10";
 
-function increaseAge(u: UserType) {
-    u.age = u.age + 1
-}
+
 
 test('reference type test', () => {
     let user: UserType = {
         name: 'Andrei',
-        age: 33,
+        hair: 32,
         address: {
-            title: 'Minsk'
+            city: 'Minsk'
         }
     }
 
-    increaseAge(user)
+    const cutUser = makeHairStyle(user, 2)
 
-    expect(user.age).toBe(34)
+    expect(user.hair).toBe(32)
+    expect(cutUser.hair).toBe(16)
+    expect(cutUser.address).toBe(user.address)
+
+})
+
+test('change address', () => {
+    let user: UserWithLaptopType = {
+        name: 'Andrei',
+        hair: 32,
+        address: {
+            city: 'Minsk',
+            house: 12
+        },
+        laptop: {
+            title: 'ASUS'
+        }
+    }
+
+    const movedUser = moveUser(user, 'Vitebsk')
+
+     expect(user).not.toBe(movedUser)
+    expect(user.address).not.toBe(movedUser.address)
+    expect(user.laptop).toBe(movedUser.laptop)
+    expect(movedUser.address.city).toBe('Vitebsk')
+
+})
+test('upgrade laptop to Macbook', () => {
+    let user: UserWithLaptopType = {
+        name: 'Andrei',
+        hair: 32,
+        address: {
+            city: 'Minsk',
+            house: 12
+        },
+        laptop: {
+            title: 'ASUS'
+        }
+    }
+
+    const userWithMacbook = upgradeUserLaptop(user, 'Macbook')
+
+    expect(user).not.toBe(userWithMacbook)
+    expect(user.address).toBe(userWithMacbook.address)
+    expect(user.laptop).not.toBe(userWithMacbook.laptop)
+    expect(userWithMacbook.laptop.title).toBe('Macbook')
+    expect(user.laptop.title).toBe('ASUS')
+
+})
+
+test('upgrade laptop to Macbook', () => {
+    let user: UserWithLaptopType & UserWithBooksType = {
+        name: 'Andrei',
+        hair: 32,
+        address: {
+            city: 'Minsk',
+            house: 12
+        },
+        laptop: {
+            title: 'ASUS'
+        },
+        books: ['css','html', 'js', 'react']
+    }
+
+    const userCopy = moveUserToOtherHouse(user, 84)
+
+    expect(user).not.toBe(userCopy)
+    expect(user.books).toBe(userCopy.books)
+    expect(user.laptop).toBe(userCopy.laptop)
+    expect(user.address).not.toBe(userCopy.address)
+    expect(userCopy.address.house).toBe(84)
+
 })
--- src/10-immutability/10.tsx
+++ src/10-immutability/10.tsx
@@ -1,7 +1,38 @@
 export type UserType = {
     name: string
-    age: number
+    hair: number
     address: {
-        title: string
+        city: string,
+        house?: number
     }
+}
+export type LaptopType = {
+    title: string
+}
+export type UserWithLaptopType = UserType & {
+    laptop: LaptopType
+}
+
+export type UserWithBooksType = UserType & {
+    books: string[]
+}
+
+export function makeHairStyle(u: UserType, power: number) {
+    const copyU = {...u, hair: u.hair / power}
+    //copyU.hair = u.hair / power
+    return copyU
+}
+
+export function moveUser(u: UserWithLaptopType, newCity: string) {
+    const copyU = {...u, address: {...u.address, city: newCity}}
+
+    return copyU
+}
+
+export function moveUserToOtherHouse(u: UserWithLaptopType & UserWithBooksType, newHouse: number) {
+return {...u, address: {...u.address, house: newHouse}}
+}
+
+export function upgradeUserLaptop(u: UserWithLaptopType, newLaptop: string) {
+   return {...u, laptop: {...u.laptop, title: newLaptop}}
 }
