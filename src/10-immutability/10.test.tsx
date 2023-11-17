ac3ae3349bbd1998cf658c06767fc6181dec6b5e
Andrei Sheleg <andrej.sheleg@gmail.com>
2023-11-17 11:18:09 +0300

    add all immutability test

--- src/10-immutability/10.test.tsx
+++ src/10-immutability/10.test.tsx
@@ -1,11 +1,12 @@
 import {
+    addNewBooksToUser, addNewCompany,
     makeHairStyle,
     moveUser,
-    moveUserToOtherHouse,
+    moveUserToOtherHouse, removeBook, updateBook, updateCompanyTitle, updateCompanyTitleAssociative,
     upgradeUserLaptop,
     UserType,
     UserWithBooksType,
-    UserWithLaptopType
+    UserWithLaptopType, WithCompaniesType
 } from "./10";
 
 
@@ -92,5 +93,139 @@ test('upgrade laptop to Macbook', () => {
     expect(user.laptop).toBe(userCopy.laptop)
     expect(user.address).not.toBe(userCopy.address)
     expect(userCopy.address.house).toBe(84)
+
+})
+
+test('add new books to user', () => {
+    let user: UserWithLaptopType & UserWithBooksType = {
+        name: 'Andrei',
+        hair: 32,
+        address: {
+            city: 'Minsk',
+            house: 12
+        },
+        laptop: {
+            title: 'ASUS'
+        },
+        books: ['css','html', 'js', 'react']
+    }
+
+    const userCopy = addNewBooksToUser(user, ['ts', 'rest API'])
+
+    expect(user).not.toBe(userCopy)
+    expect(user.laptop).toBe(userCopy.laptop)
+    expect(user.address).toBe(userCopy.address)
+    expect(user.books).not.toBe(userCopy.books)
+    expect(userCopy.books[4]).toBe('ts')
+    expect(userCopy.books[5]).toBe('rest API')
+
+})
+
+test('update js to ts', () => {
+    let user: UserWithLaptopType & UserWithBooksType = {
+        name: 'Andrei',
+        hair: 32,
+        address: {
+            city: 'Minsk',
+            house: 12
+        },
+        laptop: {
+            title: 'ASUS'
+        },
+        books: ['css','html', 'js', 'react']
+    }
+
+    const userCopy = updateBook(user, 'js', 'ts')
+
+    expect(user).not.toBe(userCopy)
+    expect(user.laptop).toBe(userCopy.laptop)
+    expect(user.address).toBe(userCopy.address)
+    expect(user.books).not.toBe(userCopy.books)
+    expect(userCopy.books[2]).toBe('ts')
+    expect(user.books.length).toBe(4)
+
+})
+
+test('remove js book', () => {
+    let user: UserWithLaptopType & UserWithBooksType = {
+        name: 'Andrei',
+        hair: 32,
+        address: {
+            city: 'Minsk',
+            house: 12
+        },
+        laptop: {
+            title: 'ASUS'
+        },
+        books: ['css','html', 'js', 'react']
+    }
+
+    const userCopy = removeBook(user, 'js')
+
+    expect(user).not.toBe(userCopy)
+    expect(user.laptop).toBe(userCopy.laptop)
+    expect(user.address).toBe(userCopy.address)
+    expect(user.books).not.toBe(userCopy.books)
+    expect(userCopy.books[2]).toBe('react')
+})
+
+test('add Google to companies', () => {
+    let user: UserWithLaptopType & WithCompaniesType = {
+        name: 'Andrei',
+        hair: 32,
+        address: {
+            city: 'Minsk',
+            house: 12
+        },
+        laptop: {
+            title: 'ASUS'
+        },
+        companies: [{id:1, title: 'Bel-Telecart'}, {id:2, title: 'SkyEnergo'}]
+    }
+
+    const userCopy = addNewCompany(user, {id: 3, title: 'Googlee'})
+
+    expect(user).not.toBe(userCopy)
+    expect(user.laptop).toBe(userCopy.laptop)
+    expect(user.address).toBe(userCopy.address)
+    expect(user.companies).not.toBe(userCopy.companies)
+    expect(userCopy.companies[2].title).toBe('Googlee')
+
+})
+
+test('update Епам to EPAM', () => {
+    let user: UserWithLaptopType & WithCompaniesType = {
+        name: 'Andrei',
+        hair: 32,
+        address: {
+            city: 'Minsk',
+            house: 12
+        },
+        laptop: {
+            title: 'ASUS'
+        },
+        companies: [{id:1, title: 'Епам'}, {id:2, title: 'SkyEnergo'}]
+    }
+
+    const userCopy = updateCompanyTitle(user, 1, 'EPAM')
+
+    expect(user).not.toBe(userCopy)
+    expect(user.companies).not.toBe(userCopy.companies)
+    expect(userCopy.companies[0].title).toBe('EPAM')
+
+})
+
+test('update company', () => {
+
+    let companies = {
+        'Andrei': [{id:1, title: 'Bel-Telecart'}, {id:2, title: 'SkyEnergo'}],
+        'Dimych': [{id:1, title: 'Epam'}, {id:2, title: 'IT-Incubator'}]
+    }
+
+    const copy = updateCompanyTitleAssociative(companies, 'Dimych', 1, 'EPAM')
+
+    expect(copy['Andrei']).toBe(companies['Andrei'])
+    expect(copy['Dimych']).not.toBe(companies['Dimych'])
+    expect(copy['Dimych'][0].title).toBe('EPAM')
 
 })
--- src/10-immutability/10.tsx
+++ src/10-immutability/10.tsx
@@ -17,6 +17,13 @@ export type UserWithBooksType = UserType & {
     books: string[]
 }
 
+export type CompanyType = {
+    id: number, title: string
+}
+export type WithCompaniesType = {
+    companies: CompanyType[]
+}
+
 export function makeHairStyle(u: UserType, power: number) {
     const copyU = {...u, hair: u.hair / power}
     //copyU.hair = u.hair / power
@@ -30,9 +37,43 @@ export function moveUser(u: UserWithLaptopType, newCity: string) {
 }
 
 export function moveUserToOtherHouse(u: UserWithLaptopType & UserWithBooksType, newHouse: number) {
-return {...u, address: {...u.address, house: newHouse}}
+    return {...u, address: {...u.address, house: newHouse}}
 }
 
 export function upgradeUserLaptop(u: UserWithLaptopType, newLaptop: string) {
-   return {...u, laptop: {...u.laptop, title: newLaptop}}
+    return {...u, laptop: {...u.laptop, title: newLaptop}}
 }
+
+export function addNewBooksToUser(u: UserWithLaptopType & UserWithBooksType, newBooks: string[]) {
+    return {...u, books: [...u.books, ...newBooks]}
+}
+
+export function updateBook(u: UserWithLaptopType & UserWithBooksType, oldBook: string, updateBook: string) {
+    return {...u, books: u.books.map(el => el === oldBook ? updateBook : el)}
+}
+
+export const removeBook = (u: UserWithLaptopType & UserWithBooksType, bookForDelete: string) => (
+    {...u, books: u.books.filter(el => el !== bookForDelete)}
+)
+
+export const addNewCompany = (u: UserWithLaptopType & WithCompaniesType, newCompany: { id: number, title: string }) => (
+    {...u, companies: [...u.companies, newCompany]}
+)
+
+export const updateCompanyTitle = (u: WithCompaniesType, companuId: number, newTitle: string) => (
+    {
+        ...u, companies: u.companies.map(
+            el => el.id === companuId ? {...el, title: newTitle} : el)
+    }
+)
+
+export const updateCompanyTitleAssociative =
+    (companies: { [key: string]: CompanyType[] },
+     userName: string,
+     companyId: number,
+     newTitle: string) => (
+        {
+            ...companies, [userName]: companies[userName].map(el => el.id === companyId ?
+                {...el, title: newTitle} : el)
+        }
+    )
